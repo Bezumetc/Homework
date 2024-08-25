00fe8990d1ed7b6e622070a92538a6ecea15a95f
Bezumetc <ggle.mudak@gmail.com>
2024-08-25 20:22:29 +0000

    Homework

---
+++ Variable.py
@@ -0,0 +1,5 @@
+homework_count = 12
+time_count = 1.5
+name = 'Python'
+average_time = 'среднее время выполнения:'
+print('Курс: ',name,', ','всего задач:',homework_count,', ','затрачено часов:',time_count,', ',average_time,time_count/homework_count,'.',sep='')
---
+++ str.py
@@ -0,0 +1,7 @@
+#Строки и индексация строк
+word = 'Топинамбур'
+print(word[0])
+print(word[-1])
+print(word[5:])
+print(word[::-1])
+print(word[1::2])
