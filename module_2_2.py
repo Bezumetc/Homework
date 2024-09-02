096e5721d2dc17292463841210cd0fe133814568
Bezumetc <ggle.mudak@gmail.com>
2024-09-02 23:10:30 +0000

    Homework

--- module_2_2.py
+++ module_2_2.py
@@ -1,9 +1,9 @@
-first=int(input('Введите 1ое число: '))
-second=int(input('Введите 2ое число: '))
-third=int(input('Введите 3е число: '))
-if first==second and first==third:
+first = int(input('Введите 1ое число: '))
+second = int(input('Введите 2ое число: '))
+third = int(input('Введите 3е число: '))
+if first == second and first == third:
     print(3)
-elif first==second or first==third or second==third:
+elif first == second or first == third or second == third:
     print(2)
 else:
     print(0)
