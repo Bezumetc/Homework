da74d30f1623bf9931af447883d74fe7a00814cb
Bezumetc <ggle.mudak@gmail.com>
2024-09-18 16:29:12 +0000

    Homework

--- module_3_1.py
+++ module_3_1.py
@@ -1,9 +1,17 @@
-#Динамическая типизация
-name = 'Vladimir'
-print(name)
-age = 32
-print(age)
-new_age = age+1
-print (new_age)
-is_student = True
-print(is_student)
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+def string_info(string):
+    print(len(string), string.upper(), string.lower())
+    count_calls()
+    return string
+def is_contains(string, list_to_search):
+    count_calls()
+    return string.upper() in [s.upper() for s in list_to_search]
+
+string_info('Capybara')
+string_info('Armageddon')
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
