5d3a43fa24addf2b07da68a24e2706ee061d0b77
Bezumetc <ggle.mudak@gmail.com>
2025-01-21 17:30:02 +0000

    Homework

---
+++ module_7_3.py
@@ -0,0 +1,42 @@
+import string
+
+
+class WordsFinder:
+    def __init__(self, *file_names):
+        self.file_names = file_names  
+
+    def get_all_words(self):
+        all_words = {}
+
+        for file_name in self.file_names:
+                with open(file_name, 'r', encoding='utf-8') as file:
+                    text = file.read().lower()
+                    punctuation = [',', '.', '=', '!', '?', ';', ':']
+                    for punct in punctuation:
+                        text = text.replace(punct, '')
+                        text = text.replace(' - ', ' ')
+                    words = text.split()
+                    all_words[file_name] = words
+
+        return all_words
+
+    def find(self, word):
+        results = {}
+        for file_name, words in self.get_all_words().items():
+            if word.lower() in words:
+                results[file_name] = words.index(word.lower()) + 1
+
+        return results
+
+    def count(self, word):
+        counters = {}
+        for value, key in self.get_all_words().items():
+            words_count = key.count(word.lower())
+            counters[value] = words_count
+
+        return counters
+
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words())  # Все слова
+print(finder2.find('TEXT'))  # 3 слово по счёту
+print(finder2.count('teXT'))  # 4 слова teXT в тексте всего
